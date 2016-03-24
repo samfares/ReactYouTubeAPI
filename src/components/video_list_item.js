9bb50be0cd221173255ff8dc2259921aa1f93bd1
Sam Fares <oalfares@beachbody.com>
2016-03-24 15:04:29 -0700

    add folders

---
+++ src/components/video_list_item.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const VideoListItem =  ({video, onVideoSelect}) => {
+  const imageURL = video.snippet.thumbnails.default.url;
+  const videoTitle = video.snippet.title;
+  return (
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
+      <div className="video_list_item">
+        <div className="media-left">
+          <img className="media-object" src={imageURL} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">
+            {videoTitle}
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoListItem;
